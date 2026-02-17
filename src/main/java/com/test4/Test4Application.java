943276bfde31b30e34d51f5842e853378d5ee385
SANJAY K L <79360102+Sanjaykanur@users.noreply.github.com>
2026-02-17 16:44:46 +0530

    Update Test4Application.java
    first chnge

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -8,7 +8,7 @@ public class Test4Application {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome");
-		x=10;
+		System.out.println("Welcome");
 		SpringApplication.run(Test4Application.class, args);
 	}
 
