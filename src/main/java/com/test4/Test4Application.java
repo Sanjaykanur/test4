84dde510954a95cfbcd90b1070e7f78e2cfa549e
Sanjaykanur <sanjaykumar8667349532@gmail.com>
2026-02-18 17:14:32 +0530

    First Commit change
    This reverts commit 50432be9f39336c082a974c88fd3128268217ab1.

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Boot Application");
 		System.out.println("Welcome");
 		System.out.println("Welcome");
 		int x =10;
